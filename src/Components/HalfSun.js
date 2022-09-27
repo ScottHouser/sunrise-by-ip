1bb423347338a47095f94a0124ac4889f930ab36
Scott Houser <Scottanthonyhouser@gmail.com>
2022-09-27 00:34:35 -0600

    chore: removing seconds

--- src/Components/HalfSun.js
+++ src/Components/HalfSun.js
@@ -1,17 +1,30 @@
 export default function HalfSun(props) {
 
+  const trimTime = (input) => {
+    if( input ){
+      let array = input.split('')
+      let whereToTrim = array.lastIndexOf(':')  
+  
+      array.splice(whereToTrim, 3)
+      return array.join('')
+
+    }else{
+      return('')
+    }
+  }
+
   const textOrderDisplay = () => {
     if(props.isSunUp){
       return(
         <>
           <p className='sun-label'>{props.title}</p>
-          <h1 className='time-display'>{props.timeDisplay}</h1>
+          <h1 className='time-display'>{trimTime(props.timeDisplay)}</h1>
         </>
       )
       }else{
         return(
           <>
-            <h1 className='time-display'>{props.timeDisplay}</h1>
+            <h1 className='time-display'>{trimTime(props.timeDisplay)}</h1>
             <p className='sun-label'>{props.title}</p>
           </>
         )
